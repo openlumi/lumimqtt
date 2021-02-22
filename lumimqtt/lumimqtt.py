6e807d03faf1ab69b41f116f0f273bae7756b6c2
CODeRUS <coderusinbox@gmail.com>
2021-02-22 22:40:40 +0300

    Apply transision timeout to light

--- lumimqtt/lumimqtt.py
+++ lumimqtt/lumimqtt.py
@@ -65,13 +65,18 @@ class Light(Device):
         self.led_g = self.device['g']
         self.led_b = self.device['b']
         self.pwm_max = int(self.device['pwm_max'])
+
+        state_r = int(open(self.led_r, 'r').read())
+        state_g = int(open(self.led_g, 'r').read())
+        state_b = int(open(self.led_b, 'r').read())
+
         self.state = {
-            'state': 'OFF',
+            'state': 'ON' if state_r or state_g or state_b else 'OFF',
             'brightness': 255,
             'color': {
-                'r': 255,
-                'g': 255,
-                'b': 255,
+                'r': int(state_r / self.pwm_max * 255),
+                'g': int(state_g / self.pwm_max * 255),
+                'b': int(state_b / self.pwm_max * 255),
             },
         }
 
@@ -79,7 +84,7 @@ class Light(Device):
     def topic_set(self):
         return f'{self.topic}/set'
 
-    async def set(self, value: dict):
+    async def write(self, value: dict):
         state = value.get('state', self.state['state'])
         color = value.get('color', self.state['color'])
         brightness = value.get('brightness', self.state['brightness'])
@@ -98,6 +103,67 @@ class Light(Device):
             with open(file, 'w+') as f:
                 f.write(str(pwm_value))
                 f.write('\n')
+
+    async def set(self, value: dict):
+        state = value.get('state', self.state['state'])
+        color = value.get('color', self.state['color'])
+        brightness = value.get('brightness', self.state['brightness'])
+        current_brightness = self.state['brightness']
+        target_brightness = brightness
+        if self.state['state'].lower() == 'off':
+            current_brightness = 0
+            if color['r'] == 0 and color['g'] == 0 and color['b'] == 0:
+                color['r'] = 255
+                color['g'] = 255
+                color['b'] = 255
+        if state.lower() == 'off':
+            target_brightness = 0
+        transition = value.get('transition', 1)
+
+        if transition:
+            start_level = current_brightness
+
+            """ Use brightness or convert brightness_pct """
+            end_level = int(target_brightness)
+            
+            if start_level == end_level:
+                await self.write(value)
+                self.state = {
+                    'state': state,
+                    'brightness': brightness,
+                    'color': color,
+                }
+                return;
+
+            """ Calculate number of steps """
+            steps = abs(start_level - end_level)
+            fadeout = start_level > end_level
+
+            """ Calculate the delay time """
+            delay = transition / steps / 3
+            step_by = 1
+
+            new_level = start_level
+            for x in range(steps):
+                if fadeout:
+                    new_level = new_level - step_by
+                    if new_level < end_level:
+                        new_level = end_level
+                else:
+                    new_level = new_level + step_by
+                    if new_level > end_level:
+                        new_level = end_level
+                new_value = value.copy()
+                new_value['brightness'] = new_level
+                new_value['state'] = 'ON'
+                await self.write(new_value)
+                await aio.sleep(delay)
+            if new_level != target_brightness:
+                new_value = value.copy()
+                new_value['brightness'] = target_brightness
+                await self.write(new_value)
+        else:
+            await self.write(value)
 
         self.state = {
             'state': state,
